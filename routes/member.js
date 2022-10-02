bff0850972788f6a7aa88425dda6074d22f279f2
Soobin <soobin400@gmail.com>
2022-10-02 15:52:04 +0900

    /:memberId/similar 라우터 수정(사용가능)

--- routes/member.js
+++ routes/member.js
@@ -7,34 +7,37 @@ const { off } = require('process');
 const router = express.Router();
 
 /** 유사한 의원 추천(의원 상세페이지)-4개 */
-// 쓰이지 않을 예정
 router.get('/:memberId/similar', async (req, res, next) => {
     const memberId = req.params.memberId;
 
     try {
-        // const member = await Member.findOne({ where: { id: memberId } });
+        const member = await Member.findOne({ where: { id: memberId } });
 
-        // 아직 flask 서버가 공사중
-        return res.status(200).json({ success: true, members: ['L2I9861C', 'EMC8812P'] })
+        if(member) {
+            await axios
+                .post(`${process.env.MEMBER_API_URL}`, {
+                    memberId: memberId,
+                })
+                .then(async (response) => {
+                    let members = [];
+                    
+                    for(m of response.data.members) {
+                        members.push(await Member.findOne({ where: { id: m } }));
+                    }
 
-        // if(member) {
-        //     await axios
-        //         .post(`${process.env.MEMBER_API_URL}`, {
-        //             memberId: memberId,
-        //         })
-        //         .then((response) => {
-        //            // const members = response.data.members;
-        //             console.log(response.data);
-        //             return res.status(200).json({ success: true, members: response.data.members });
-        //         });
-        // }
-        // else {
-        //     return res.status(200).json({ success: false, message: '국회의원을 찾을 수 없습니다.' });
-        // }
-    }
-    catch(e) {
-        console.log(e);
-        return res.status(200).json({ success: false });
+                    return res.status(200).json({ 
+                        success: true, 
+                        members: members, 
+                    });
+                }
+            );
+        }
+        else {
+            return res.status(200).json({ success: false, message: '국회의원을 찾을 수 없습니다.' });
+        }
+    } catch(err) {
+        console.log(err);
+        return res.status(200).json({ success: false, message: 'Error has occured' });
     }
 });
 
